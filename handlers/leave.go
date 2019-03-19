cd93fe2a757187257e9663eeb5cd57c6b46a0125
VineBalloon <vincentchen2.033@gmail.com>
2019-03-19 14:58:42 +1100

    Need to fix

--- handlers/leave.go
+++ handlers/leave.go
@@ -1,6 +1,8 @@
 package handlers
 
 import (
+	"errors"
+
 	"github.com/VineBalloon/nozobot/client"
 	"github.com/VineBalloon/nozobot/helpers"
 )
@@ -8,11 +10,16 @@ import (
 // Leave
 // The command to leave voice gracefully
 type Leave struct {
-	Name string
+	name        string
+	description string
+}
+
+func (p *Leave) Name() string {
+	return p.name
 }
 
 func (p *Leave) Desc() string {
-	return "Leaves all audio in a pinchi :sparkling_heart:"
+	return p.description
 }
 
 func (p *Leave) Roles() []string {
@@ -25,15 +32,24 @@ func (p *Leave) Channels() []string {
 
 // Handle
 // Gracefully closes all encoding, stream, and voice connections
-func (p *Leave) Handle(cs *client.ClientState) error {
+func (p *Leave) MsgHandle(cs *client.ClientState) error {
 	s := cs.Session
 	m := cs.Message
+
+	// Check voice channel
+	if cs.Voice == nil {
+		return errors.New("leave: no voice room to leave")
+	}
+
+	// Get voice channel info
 	channel, err := cs.Session.Channel(cs.Voice.Id)
 	if err != nil {
 		return err
 	}
 
 	// Leave the voice connection
+	//cs.Voice.Stop()
+	//cs.Voice.Connection.Close()
 	err = cs.Voice.Leave()
 	if err != nil {
 		return err
@@ -51,5 +67,6 @@ func (p *Leave) Handle(cs *client.ClientState) error {
 func NewLeave() *Leave {
 	return &Leave{
 		"Leave",
+		"Leaves all audio in a pinchi :sparkling_heart:",
 	}
 }
