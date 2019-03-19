fdc00bc859a9d1b7b8a9be3277cd957210fa352f
VineBalloon <vincentchen2.033@gmail.com>
2019-03-19 14:56:11 +1100

    Added Name method

--- handlers/handler.go
+++ handlers/handler.go
@@ -5,8 +5,16 @@ import "github.com/VineBalloon/nozobot/client"
 // Handler
 // The interface for all commands
 type Handler interface {
-	Desc() string                     /* Desc returns the description of the command */
-	Roles() []string                  /* Roles returns the permitted roles for the command */
-	Channels() []string               /* Channels returns the permitted channels */
-	Handle(*client.ClientState) error /* Handle handles message events */
+	Name() string       /* Name returns the name of the command */
+	Desc() string       /* Desc returns the description of the command */
+	Roles() []string    /* Roles returns the permitted roles for the command */
+	Channels() []string /* Channels returns the permitted channels */
+
+	MsgHandle(*client.ClientState) error /* Handle handles message events */
+	// Add more event handlers here
+}
+
+// Initialise
+func init() {
+
 }
