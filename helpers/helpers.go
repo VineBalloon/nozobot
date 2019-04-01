48c8bb8d223471d7eba013469d8fea74d048881a
VineBalloon <vincentchen2.033@gmail.com>
2019-04-02 02:13:44 +1100

    Added stringinslice func, removed surround

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -1,40 +1,46 @@
 package helpers
 
-func Surround(s string, d string) string {
-	out := d + s
-	c := []byte(d)
-
-	for i := len(c)/2 - 1; i >= 0; i-- {
-		opp := len(c) - 1 - i
-		c[i], c[opp] = c[opp], c[i]
-	}
-	return out + string(c)
-}
-
+// Italics Surrounds with italics escapes
 func Italics(s string) string {
-	return Surround(s, "*")
+	return "*" + s + "*"
 }
 
+// Bold Surrounds with bold escapes
 func Bold(s string) string {
-	return Surround(s, "**")
+	return "**" + s + "**"
 }
 
+// Code Surrounds with code escapes
 func Code(s string) string {
-	return Surround(s, "`")
+	return "`" + s + "`"
 }
 
+// Spoiler Surrounds with spoiler escapes
 func Spoiler(s string) string {
-	return Surround(s, "||")
+	return "||" + s + "||"
 }
 
+// Noembed Surrounds with no-embed escapes
 func Noembed(s string) string {
 	return "<" + s + ">"
 }
 
+// At Surrounds ID with @ escapes
 func At(s string) string {
 	return "<@" + s + ">"
 }
 
+// Chan Surrounds ID with chan escapes
 func Chan(s string) string {
 	return "<#" + s + ">"
+}
+
+// Stringinslice Checks if string is in the slice
+func Stringinslice(str string, slice []string) bool {
+	for _, s := range slice {
+		if s == str {
+			return true
+		}
+	}
+	return false
 }
